aee62d7bf356e733a908c0414edce6d7708046be
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-15 23:08:44 +0700

    Disable code splitting in development mode

--- src/components/core/Routes/index.jsx
+++ src/components/core/Routes/index.jsx
@@ -2,16 +2,14 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import withLoadable from 'HOCs/common/withLoadable';
-
 // Constants
 import PATHS from 'constants/router/paths';
 
 // Screens
-const Nearby = withLoadable(() => import('screens/main/Nearby'));
-const Search = withLoadable(() => import('screens/main/Search'));
-const Profile = withLoadable(() => import('screens/main/Profile'));
-const NotFound = withLoadable(() => import('screens/main/404'));
+import Nearby from 'screens/main/Nearby';
+import Search from 'screens/main/Search';
+import Profile from 'screens/main/Profile';
+import NotFound from 'screens/main/404';
 
 // Component
 const Routes = () => (
