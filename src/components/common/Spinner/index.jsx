1e3ce58055977ed751ae45950f7d7a710fe6e6cf
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-16 20:11:44 +0700

    Refactor Spinner component

--- src/components/common/Spinner/index.jsx
+++ src/components/common/Spinner/index.jsx
@@ -1,15 +1,48 @@
 // @flow
 // Module dependencies
+import { FadingCircle, ThreeBounce } from 'better-react-spinkit';
 import * as React from 'react';
 
-// Styles
+// Companion files
 import './styles.scss';
 
-// Types
+// Static types
+type Props = { loading: boolean };
 type Return = React.Node;
 
+// Default props
+const defaultProps = {
+  loading: false
+};
+
+// Options
+const options = {
+  bounce: {
+    color: '#999',
+    duration: '1.25s',
+    gutter: 6,
+    size: 6
+  }
+};
+
 // Component
-const Spinner = (): Return => <div styleName="spinner">Loading...</div>;
+const Spinner = ({ loading }: Props): Return => (
+  <Choose>
+    <When condition={loading}>
+      <div styleName="bounce">
+        <ThreeBounce {...options.bounce} />
+      </div>
+    </When>
+    <Otherwise>
+      <div styleName="circle">
+        <FadingCircle />
+      </div>
+    </Otherwise>
+  </Choose>
+);
+
+// Specify default values for props
+Spinner.defaultProps = defaultProps;
 
 // Module exports
 export default Spinner;
