f0e7c061cdb95baa12161f6380202775a22f92b7
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-17 21:27:54 +0700

    Refactor Nearby screen by extracting container & UI components

--- src/screens/main/Nearby/index.jsx
+++ src/screens/main/Nearby/index.jsx
@@ -1,38 +1,11 @@
-// @flow
 // Module dependencies
-import * as React from 'react';
+import React from 'react';
 
-import Layout from 'components/common/Layout';
-import { Body, Document, Head, Title } from 'components/common/Page';
-
-// Styles
-import './styles.scss';
-
-// Types
-type Return = React.Node;
+// Relative files
+import UI from './UI';
 
 // Component
-const Nearby = (): Return => (
-  <Document>
-    <Head>
-      <Title>Nearby | Search Flow</Title>
-    </Head>
-    <Body>
-      <Layout>
-        <h2 className="headline">Nearby</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industrys standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
-        </p>
-      </Layout>
-    </Body>
-  </Document>
-);
+const Nearby = () => <UI />;
 
 // Module exports
 export default Nearby;
