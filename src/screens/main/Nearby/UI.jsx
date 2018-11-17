14ac8d87f2481f4e932bbf13ab7e24539192af3d
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-17 21:36:02 +0700

    Add helper method for redirecting to Search screen

--- src/screens/main/Nearby/UI.jsx
+++ src/screens/main/Nearby/UI.jsx
@@ -4,11 +4,20 @@ import * as React from 'react';
 import Layout from 'components/common/Layout';
 import { Body, Document, Head, Title } from 'components/common/Page';
 
+// Constants
+import PATHS from 'constants/router/paths';
+
 // Styles
 import './styles.scss';
 
 // Component
 class Nearby extends React.Component {
+  // Redirect to search screen
+  redirectSearch = () => {
+    this.props.history.push({ pathname: PATHS.main.search });
+  };
+
+  // Render component
   render() {
     return (
       <Document>
@@ -20,6 +29,7 @@ class Nearby extends React.Component {
             <div styleName="section">
               <input
                 className="btn btn-outline-secondary btn-block"
+                onClick={this.redirectSearch}
                 type="button"
                 value="What are you looking for?"
               />
