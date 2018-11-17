b5ccff9eb119a43f48a205f9c170db3755a7d927
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-17 20:22:41 +0700

    Create Result list component

---
+++ src/components/common/Result/List/index.jsx
@@ -0,0 +1,33 @@
+// Module dependencies
+import React from 'react';
+
+// Relative components
+import Item from '../Item';
+
+// Styles
+import './styles.scss';
+
+// Component
+class List extends React.Component {
+  // Render list item
+  renderItem = item => <Item item={item} key={item.id} onClick={this.props.onSelect} />;
+
+  // Render component
+  render() {
+    // Variables
+    const { data, location, name } = this.props;
+
+    // Return elements
+    return (
+      <div styleName="wrapper">
+        <h2 styleName="headline">
+          Search results {name && `of ${name}`} {location && `in ${location}`}
+        </h2>
+        <div>{data.map(this.renderItem)}</div>
+      </div>
+    );
+  }
+}
+
+// Module exports
+export default List;
