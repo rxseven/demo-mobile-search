17e5db6ec04b83b597e3761752a8d267bd215e31
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-17 21:35:19 +0700

    Add search box to Nearby screen

--- src/screens/main/Nearby/UI.jsx
+++ src/screens/main/Nearby/UI.jsx
@@ -8,27 +8,28 @@ import { Body, Document, Head, Title } from 'components/common/Page';
 import './styles.scss';
 
 // Component
-const UI = () => (
-  <Document>
-    <Head>
-      <Title>Nearby | Search Flow</Title>
-    </Head>
-    <Body>
-      <Layout>
-        <h2 className="headline">Nearby</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industrys standard dummy text ever since the 1500s, when an unknown printer took
-          a galley of type and scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
-          Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
-          PageMaker including versions of Lorem Ipsum.
-        </p>
-      </Layout>
-    </Body>
-  </Document>
-);
+class Nearby extends React.Component {
+  render() {
+    return (
+      <Document>
+        <Head>
+          <Title>Nearby | Search Flow</Title>
+        </Head>
+        <Body>
+          <Layout>
+            <div styleName="section">
+              <input
+                className="btn btn-outline-secondary btn-block"
+                type="button"
+                value="What are you looking for?"
+              />
+            </div>
+          </Layout>
+        </Body>
+      </Document>
+    );
+  }
+}
 
 // Module exports
-export default UI;
+export default Nearby;
