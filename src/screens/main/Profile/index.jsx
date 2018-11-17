9d6ab7f30a6c4acba31fc302ae5fd32b2ce8cc65
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-17 21:03:23 +0700

    Update headline styles on Profile screen

--- src/screens/main/Profile/index.jsx
+++ src/screens/main/Profile/index.jsx
@@ -19,7 +19,7 @@ const Profile = (): Return => (
     </Head>
     <Body>
       <Layout>
-        <h2 className="headline">Profile</h2>
+        <h2 styleName="headline">Profile</h2>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
           been the industrys standard dummy text ever since the 1500s, when an unknown printer took
