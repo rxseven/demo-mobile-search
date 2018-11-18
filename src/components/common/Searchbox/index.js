482b8dd553be82fb396846df18b403a544997319
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-18 22:07:31 +0700

    Create SearchboxContainer component

--- src/components/common/Searchbox/index.js
+++ src/components/common/Searchbox/index.js
@@ -1,2 +1,3 @@
 export { default as SearchboxBody } from './SearchboxBody';
+export { default as Searchbox } from './SearchboxContainer';
 export { default as SearchboxForm } from './SearchboxForm';
