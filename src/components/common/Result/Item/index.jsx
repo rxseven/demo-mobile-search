149d6a8a5101b644f3217e231ef05b97cc525aa8
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-18 14:00:33 +0700

    Add location property to Result index component

--- src/components/common/Result/Item/index.jsx
+++ src/components/common/Result/Item/index.jsx
@@ -5,13 +5,20 @@ import React from 'react';
 import './styles.scss';
 
 // Component
-const Item = props => (
-  <div className="card bg-light mb-3">
-    <div className="card-body">
-      <p className="card-text">{props.item.name}</p>
+const Item = (props) => {
+  // Variables
+  const { name, location } = props.item;
+
+  // View
+  return (
+    <div className="card bg-light mb-3">
+      <div className="card-body">
+        <p className="card-text">{name}</p>
+      </div>
+      <div className="card-footer text-muted">{location}</div>
     </div>
-  </div>
-);
+  );
+};
 
 // Module exports
 export default Item;
