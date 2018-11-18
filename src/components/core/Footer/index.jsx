d681bd0f93f87adfd4bb67a6f46e42835ad58140
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-18 22:03:53 +0700

    Refactor Footer’s layout structure with Fixed component

--- src/components/core/Footer/index.jsx
+++ src/components/core/Footer/index.jsx
@@ -3,8 +3,8 @@
 import * as React from 'react';
 import { NavLink } from 'react-router-dom';
 
-import { Column, Container, Row } from 'components/common/Grid';
-import Layout from 'components/common/Layout';
+// Components
+import Fixed from 'components/common/Fixed';
 
 // Constants
 import PATHS from 'constants/router/paths';
@@ -18,27 +18,21 @@ type Return = React.Node;
 // Component
 const Footer = (): Return => (
   <footer styleName="container">
-    <Container>
-      <Row>
-        <Column>
-          <Layout>
-            <ul styleName="navigation">
-              <li>
-                <NavLink exact to={PATHS.main.nearby}>
-                  Around Me
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to={PATHS.main.search}>Search</NavLink>
-              </li>
-              <li>
-                <NavLink to={PATHS.main.profile}>My Profile</NavLink>
-              </li>
-            </ul>
-          </Layout>
-        </Column>
-      </Row>
-    </Container>
+    <Fixed>
+      <ul styleName="navigation">
+        <li>
+          <NavLink exact to={PATHS.main.nearby}>
+            Around Me
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to={PATHS.main.search}>Search</NavLink>
+        </li>
+        <li>
+          <NavLink to={PATHS.main.profile}>My Profile</NavLink>
+        </li>
+      </ul>
+    </Fixed>
   </footer>
 );
 
