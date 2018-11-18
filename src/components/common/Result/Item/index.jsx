58a9c08cdc398e2d2d1f93f1c04429df7bb35874
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-18 15:23:02 +0700

    Refactor content structure in Result item component

--- src/components/common/Result/Item/index.jsx
+++ src/components/common/Result/Item/index.jsx
@@ -7,13 +7,20 @@ import './styles.scss';
 // Component
 const Item = (props) => {
   // Variables
-  const { name, location } = props.item;
+  const { category, name, location } = props.item;
 
   // View
   return (
     <div className="card bg-light mb-3">
       <div className="card-body">
-        <p className="card-text">{name}</p>
+        <h5 className="card-title" styleName="headline">
+          {name}
+        </h5>
+        <ul styleName="details">
+          <li styleName="txt-category">{category}</li>
+          <li styleName="txt-opening">Opening Hour 08:00 - 22:00</li>
+          <li styleName="txt-description">Authentic food and decoration. I..</li>
+        </ul>
       </div>
       <div className="card-footer text-muted">{location}</div>
     </div>
