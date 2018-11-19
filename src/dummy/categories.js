820dedb409e1fe98241e813c88b6c8e69aabb07c
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-19 20:30:28 +0700

    Add list of business categories

---
+++ src/dummy/categories.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    id: 'CAT001',
+    name: 'Food & Restaurant',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT002',
+    name: 'Bar & Nightlife',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT003',
+    name: 'Accommodation',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT004',
+    name: 'Small Businesses',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT005',
+    name: 'Tours & Activities',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT006',
+    name: 'Events',
+    category: false,
+    group: true
+  },
+  {
+    id: 'CAT007',
+    name: 'Beauty & Spa',
+    category: false,
+    group: true
+  }
+];
