d0a33255d952bdcb59dae8b22383d0b553c27e49
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-19 20:34:18 +0700

    Add business listing

---
+++ src/dummy/listings.js
@@ -0,0 +1,787 @@
+// Helper functions
+import { getCategory } from 'helpers/utilities';
+
+// Constants
+import CATEGORIES from './categories';
+
+export default [
+  {
+    id: 1,
+    name: 'AI Pub & Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Germany',
+    open: false
+  },
+  {
+    id: 2,
+    name: 'Amazing tour in the Nothern',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 3,
+    name: 'Aroma Spa',
+    category: getCategory(CATEGORIES, 6),
+    location: 'South Korea',
+    open: true
+  },
+  {
+    id: 4,
+    name: 'Banana IT',
+    category: getCategory(CATEGORIES, 3),
+    location: 'China',
+    open: false
+  },
+  {
+    id: 5,
+    name: 'Bangkok Motel',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 6,
+    name: 'Bionetic Spa',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Taiwan',
+    open: true
+  },
+  {
+    id: '7A',
+    name: 'Burger King, Hong Kong',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Hong Kong',
+    open: false
+  },
+  {
+    id: '7B',
+    name: 'Burger King, Thailand',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: false
+  },
+  {
+    id: '8A',
+    name: 'Bus Bar, Thailand',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '8B',
+    name: 'Bus Bar, Australia',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Australia',
+    open: true
+  },
+  {
+    id: '9A',
+    name: 'B2, Thailand',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '9B',
+    name: 'B2, Malaysia',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Malaysia',
+    open: false
+  },
+  {
+    id: 10,
+    name: 'B-Quick',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '11A',
+    name: 'Camera Hut, Thailand',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '11B',
+    name: 'Camera Hut, Indonesia',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Indonesia',
+    open: false
+  },
+  {
+    id: '12A',
+    name: 'Cat Cafe, Thailand',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '12B',
+    name: 'Cat Cafe, Japan',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 13,
+    name: 'Cheeva Spa',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Taiwan',
+    open: true
+  },
+  {
+    id: 14,
+    name: 'Chiang Mai Flower Festival',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 15,
+    name: 'Chiang Rai Food Festival',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Thailand',
+    open: true
+  },
+
+  {
+    id: 16,
+    name: 'Chomtawan Place',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Thailand',
+    open: false
+  },
+  {
+    id: 17,
+    name: 'Colorado Adventure',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Singapore',
+    open: true
+  },
+  {
+    id: 18,
+    name: 'Computer Plaza',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Germany',
+    open: false
+  },
+  {
+    id: 19,
+    name: 'Country Trail ATV',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 20,
+    name: 'Delphine Tour',
+    category: getCategory(CATEGORIES, 4),
+    location: 'France',
+    open: true
+  },
+  {
+    id: 21,
+    name: 'Doi Inthanon Trips',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: '22A',
+    name: 'Elephants Show, Switzerland',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Switzerland',
+    open: true
+  },
+  {
+    id: '22B',
+    name: 'Elephants Show, Egypt',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Egypt',
+    open: true
+  },
+  {
+    id: 23,
+    name: 'Enjoying Pub',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Ghana',
+    open: true
+  },
+  {
+    id: 24,
+    name: 'Fireworks Show',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Japan',
+    open: false
+  },
+  {
+    id: 25,
+    name: 'Four Seasons Resort',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Kenya',
+    open: true
+  },
+  {
+    id: 26,
+    name: 'Freedom Yoga',
+    category: getCategory(CATEGORIES, 6),
+    location: 'India',
+    open: false
+  },
+  {
+    id: 27,
+    name: 'Fuji Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 28,
+    name: 'Galaxy Biking',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Philippines',
+    open: false
+  },
+  {
+    id: 29,
+    name: 'Gangnam Beauty',
+    category: getCategory(CATEGORIES, 6),
+    location: 'South Korea',
+    open: true
+  },
+  {
+    id: 30,
+    name: 'Give Green Farm House Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Mexico',
+    open: true
+  },
+  {
+    id: 31,
+    name: 'Gozilla Bar',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 32,
+    name: 'Hanabi Stationary',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 33,
+    name: 'Heaven Burger',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Brazil',
+    open: true
+  },
+  {
+    id: 34,
+    name: 'Hot Girls Event',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Taiwan',
+    open: true
+  },
+  {
+    id: 35,
+    name: 'HPI Racing',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Germany',
+    open: false
+  },
+  {
+    id: 36,
+    name: 'iStudio',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Hong Kong',
+    open: true
+  },
+  {
+    id: 37,
+    name: 'iSushi',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 38,
+    name: 'IT Fair',
+    category: getCategory(CATEGORIES, 5),
+    location: 'China',
+    open: true
+  },
+  {
+    id: 39,
+    name: 'JIB Computer',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 40,
+    name: 'Juicy Pub & Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'India',
+    open: true
+  },
+  {
+    id: 41,
+    name: 'Jungle Camping',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Sweden',
+    open: true
+  },
+  {
+    id: 42,
+    name: 'Jungle Wisdom Survival Trek',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Italy',
+    open: true
+  },
+  {
+    id: 43,
+    name: 'Kao Beauty',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 44,
+    name: 'KFC Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Norway',
+    open: true
+  },
+  {
+    id: 45,
+    name: 'King Kong Mansion',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Singapore',
+    open: true
+  },
+
+  {
+    id: 46,
+    name: 'Kyosho Garage',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 47,
+    name: 'Loi Krathong Festival',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 48,
+    name: 'London Nightlife',
+    category: getCategory(CATEGORIES, 1),
+    location: 'United Kingdom',
+    open: true
+  },
+  {
+    id: 49,
+    name: 'Milk Zone',
+    category: getCategory(CATEGORIES, 0),
+    location: 'United States',
+    open: true
+  },
+  {
+    id: 50,
+    name: 'Munic Book Center',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 51,
+    name: 'M3 Telecom',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Singapore',
+    open: false
+  },
+  {
+    id: 52,
+    name: 'New Year Party',
+    category: getCategory(CATEGORIES, 5),
+    location: 'United States',
+    open: true
+  },
+  {
+    id: 53,
+    name: 'Nimman Hill',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 54,
+    name: 'Ninja Ramen',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 55,
+    name: 'Nobody Pub & Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Italy',
+    open: true
+  },
+  {
+    id: 56,
+    name: 'Northern Bike',
+    category: getCategory(CATEGORIES, 3),
+    location: 'France',
+    open: true
+  },
+  {
+    id: 57,
+    name: 'Oak & Awe Coffee House',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Australia',
+    open: true
+  },
+  {
+    id: 58,
+    name: 'October Cafe',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Brazil',
+    open: true
+  },
+  {
+    id: 59,
+    name: 'Oasis Spa Lanna',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Egypt',
+    open: true
+  },
+  {
+    id: 60,
+    name: 'OMG Beauty & Salon',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Ghana',
+    open: true
+  },
+  {
+    id: 61,
+    name: 'Orange Bar',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Kenya',
+    open: false
+  },
+  {
+    id: 62,
+    name: 'Oregano Home Cooking',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Maxico',
+    open: true
+  },
+  {
+    id: 63,
+    name: 'Pandora Resort',
+    category: getCategory(CATEGORIES, 2),
+    location: 'Norway',
+    open: true
+  },
+  {
+    id: 64,
+    name: 'Pet Mart',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 65,
+    name: 'Pizza Hut',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Taiwan',
+    open: true
+  },
+  {
+    id: 66,
+    name: 'Polar Boulangerie & Patisserie',
+    category: getCategory(CATEGORIES, 0),
+    location: 'United Kingdom',
+    open: true
+  },
+  {
+    id: 67,
+    name: 'Pranom Health Massage',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Philippines',
+    open: false
+  },
+  {
+    id: 68,
+    name: 'Qii Jazz Bar',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 69,
+    name: 'Quick Cut Barber',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 70,
+    name: 'Rimping',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: false
+  },
+  {
+    id: 71,
+    name: 'Rocky Barber',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Sweden',
+    open: true
+  },
+  {
+    id: 72,
+    name: 'Rose & Love',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Philippines',
+    open: true
+  },
+  {
+    id: 73,
+    name: 'Salad Concept',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 74,
+    name: 'Singapore Telecom',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Singapore',
+    open: true
+  },
+  {
+    id: 75,
+    name: 'Sizzler',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Germany',
+    open: false
+  },
+  {
+    id: 76,
+    name: 'Songkran Festival',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 77,
+    name: 'Space Tour',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Brazil',
+    open: true
+  },
+  {
+    id: 78,
+    name: 'Starbucks',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Australia',
+    open: false
+  },
+  {
+    id: 79,
+    name: 'Sumo Sushi',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 80,
+    name: 'Suriwong Book Center',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 81,
+    name: 'Takeshi Hair Styling',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: 82,
+    name: 'Tamiya Racing',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Japan',
+    open: false
+  },
+  {
+    id: 83,
+    name: 'Tomyamkung Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 84,
+    name: 'Tornado Wedding',
+    category: getCategory(CATEGORIES, 3),
+    location: 'United States',
+    open: true
+  },
+  {
+    id: 85,
+    name: 'Traditional Khantoke Dinner',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 86,
+    name: 'Tuk Tuk Expo',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Thailand',
+    open: false
+  },
+  {
+    id: 87,
+    name: 'Underwater Gallery',
+    category: getCategory(CATEGORIES, 3),
+    location: 'United States',
+    open: true
+  },
+  {
+    id: 88,
+    name: 'Unseen Thailand',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 89,
+    name: 'Valley and Wind',
+    category: getCategory(CATEGORIES, 3),
+    location: 'India',
+    open: true
+  },
+  {
+    id: 90,
+    name: 'Vanilla Spa',
+    category: getCategory(CATEGORIES, 6),
+    location: 'Indonesia',
+    open: false
+  },
+  {
+    id: 91,
+    name: 'Velocity Bike',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Kenya',
+    open: true
+  },
+  {
+    id: 92,
+    name: 'Walking Street',
+    category: getCategory(CATEGORIES, 5),
+    location: 'Taiwan',
+    open: true
+  },
+  {
+    id: 93,
+    name: 'Warm up Cafe',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Thailand',
+    open: false
+  },
+  {
+    id: 94,
+    name: 'Wawee Coffee',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 95,
+    name: 'White Temple',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 96,
+    name: 'Xenon Wedding',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Hong Kong',
+    open: true
+  },
+  {
+    id: 97,
+    name: 'X-Ray Tour',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Egypt',
+    open: true
+  },
+  {
+    id: 98,
+    name: 'X-Tream Trekking',
+    category: getCategory(CATEGORIES, 4),
+    location: 'South Korea',
+    open: false
+  },
+  {
+    id: 99,
+    name: 'Yahoo Buggy',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Switzerland',
+    open: true
+  },
+  {
+    id: 100,
+    name: 'Yokohama Racing Team',
+    category: getCategory(CATEGORIES, 3),
+    location: 'Japan',
+    open: true
+  },
+  {
+    id: '101A',
+    name: 'Zebra Bar, Singapore',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Singapore',
+    open: true
+  },
+  {
+    id: '101B',
+    name: 'Zebra Bar, Thailand',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 102,
+    name: 'ZEN Restaurant',
+    category: getCategory(CATEGORIES, 0),
+    location: 'Italy',
+    open: true
+  },
+  {
+    id: 103,
+    name: 'Zip Line Adventure',
+    category: getCategory(CATEGORIES, 4),
+    location: 'Thailand',
+    open: true
+  },
+  {
+    id: 104,
+    name: 'Zoo Pub',
+    category: getCategory(CATEGORIES, 1),
+    location: 'Ghana',
+    open: false
+  }
+];
