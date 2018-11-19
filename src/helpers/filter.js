0eb1a6acbeab9058b01946c5ff337e81bf658133
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-19 20:32:56 +0700

    Add helper function for filtering search terms

--- src/helpers/filter.js
+++ src/helpers/filter.js
@@ -29,3 +29,38 @@ export const filterInput = (source, { filter, value }, key) => {
   // Return the filtered results
   return character !== 0 ? source.filter(filterTest) : [];
 };
+
+// Search filter
+export const filterSearch = (source, input) => {
+  // console.log('source:', source);
+  const name = input.name.trim().toLowerCase();
+  const location = input.location.trim().toLowerCase();
+
+  // RegExp matcher
+  const matcher = (item, key, keyword) => {
+    // Variables
+    const data = item[key];
+    const term = keyword === 'my location' ? 'thailand' : keyword;
+    const { category } = item;
+
+    return category ? new RegExp(`\\b${data.toLowerCase()}\\b`).test(term) : false;
+  };
+
+  const checkName = item => matcher(item, 'name', name);
+  const checkCategory = item => matcher(item, 'category', name);
+  const checkLocation = item => matcher(item, 'location', location);
+
+  // Test each element of the array
+  const filterTest = (item) => {
+    if (!name.length) {
+      // Search by location
+      return checkLocation(item);
+    }
+
+    // Search by name and location
+    return (checkName(item) || checkCategory(item)) && checkLocation(item);
+  };
+
+  // Return the filtered results
+  return source.filter(filterTest);
+};
