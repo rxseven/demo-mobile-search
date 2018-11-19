66f7f216725002b0b6903e5549cd5678577da8c8
ME (Mac mini) <rxseven.com@gmail.com>
2018-11-19 15:39:25 +0700

    Add container styles to SearchboxContainer component

--- src/components/common/Searchbox/SearchboxContainer/index.jsx
+++ src/components/common/Searchbox/SearchboxContainer/index.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 import './styles.scss';
 
 // Component
-const SearchboxContainer = ({ children }) => <div>{children}</div>;
+const SearchboxContainer = ({ children }) => <div styleName="container">{children}</div>;
 
 // Module exportes
 export default SearchboxContainer;
